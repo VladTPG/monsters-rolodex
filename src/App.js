e33187942022aa700158da1419bbce17d2be5e4c
Vlad Cojan <cojanvladandrei@yahoo.com>
2023-10-20 15:38:31 +0300

    searchBar-done

--- src/App.js
+++ src/App.js
@@ -1,55 +1,59 @@
 import React from "react";
-import './App.css'
+import "./App.css";
 class App extends React.Component {
-
-  constructor(){
-    super()
+  constructor() {
+    super();
 
     this.state = {
-      monsters: [
-        {
-          id: 1,
-          name: "Liviu",
-        },
-        {
-          id: 2,
-          name: "Paul",
-        },
-        {
-          id: 3,
-          name: "Gabriel",
-        },
-        {
-          id: 4,
-          name: "Andrei",
-        },
-      ],
-      name:"gigel",
+      monsters: [],
+      searchField: "",
     };
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state.monsters);
+          }
+        )
+      );
+  }
+
+  search = (event) => {
+    this.setState(() => {
+      return {
+        searchField: event.target.value.toLocaleLowerCase(),
+      };
+    });
+  };
+
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div>
-        {
-          this.state.monsters.map((monster)=>{
-            return <h1 key={monster.id}>{monster.name}</h1>
-          })        
-        }
-
-        <button onClick={()=>{
-          this.setState(
-            ()=>{
-              return {
-                name:"giovani"
-              }
-            }
-          )
-        }}>
-
-        </button>  
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search monsters..."
+          onChange={(event) => {
+            this.search(event);
+          }}
+        />
+        {filteredMonsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
       </div>
     );
   }
 }
+
 export default App;
